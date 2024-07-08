69956c9991dafc92f1debb4e2813c2b82a0c2b00
thu huynh <anhthu4492@gmail.com>
2024-07-08 18:43:32 +0700

    commit lan 2

--- Automation/src/test/java/stepDenifications/MainSteps.java
+++ Automation/src/test/java/stepDenifications/MainSteps.java
@@ -11,6 +11,8 @@ public class MainSteps {
 	public void user_is_on_net_banking_landing_page() {
 	    // Write code here that turns the phrase above into concrete actions
 	    System.out.println("User is on NetBanking landing page");
+	    System.out.println("how are you?");
+	    System.out.println("what is your name?");
 	}
 	
 	@Given("User is on Practice landing page")
